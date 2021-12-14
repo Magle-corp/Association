13dfa4de625cd3cd78f54ce7d2a16bffb7ab809d
Magle-corp <wolff.kevin@protonmail.com>
2021-12-14 14:44:38 +0100

    Update e2e test

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,8 +1,8 @@
-import Test from '../../../Association-next/src/util/Test.js'
-
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    return Test(1, 2).then((result) => {
-     expect(result).to.equal(3)
-  })
-})})
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    cy.request("/");
+    cy.request("/publications");
+    cy.request("/publications/articles");
+    cy.request("/publications/evenements");
+  });
+});
