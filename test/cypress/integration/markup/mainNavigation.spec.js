88e0f9e6e3c9f2f97d10e715c5fe9b35c8eb9c96
Magle-corp <wolff.kevin@protonmail.com>
2021-12-16 20:05:06 +0100

    Add main navigation Cypress test

--- test/cypress/integration/markup/mainNavigation.spec.js
+++ test/cypress/integration/markup/mainNavigation.spec.js
@@ -16,4 +16,40 @@ describe("Main navigation", () => {
         });
       });
   });
+
+  it("Publications articles link has good text", () => {
+    cy.visit("/")
+      .get('[data-cy="link-publications-articles"] > a > span')
+      .contains("Articles");
+  });
+
+  it("Publications articles link has good redirection", () => {
+    cy.visit("/")
+      .get('[data-cy="link-publications-articles"] > a')
+      .should("have.attr", "href")
+      .and("include", "/publications/articles")
+      .then((href) => {
+        cy.request(href).then((resp) => {
+          expect(resp.status).to.eq(200);
+        });
+      });
+  });
+
+  it("Publications evenement link has good text", () => {
+    cy.visit("/")
+      .get('[data-cy="link-publications-evenements"] > a > span')
+      .contains("Evenements");
+  });
+
+  it("Publications evenement link has good redirection", () => {
+    cy.visit("/")
+      .get('[data-cy="link-publications-evenements"] > a')
+      .should("have.attr", "href")
+      .and("include", "/publications/evenements")
+      .then((href) => {
+        cy.request(href).then((resp) => {
+          expect(resp.status).to.eq(200);
+        });
+      });
+  });
 });
