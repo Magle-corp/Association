80af0c79a36b2d9f0705c1aa0a88a8ff13fdc311
Magle-corp <wolff.kevin@protonmail.com>
2021-12-18 23:32:16 +0100

    Add Cypress links tests

--- test/cypress/integration/link/links.js
+++ test/cypress/integration/link/links.js
@@ -1,5 +1,5 @@
 describe("Links", () => {
-  it("Main navigation links redirection", () => {
+  it("Homepage links", () => {
     cy.visit("/")
       .get('[data-cy="link"] > a')
       .each((link) => {
@@ -13,11 +13,11 @@ describe("Links", () => {
       });
   });
 
-  it("Main navigations links text", () => {
+  it("Homepage links text", () => {
     cy.visit("/").get('[data-cy="link"] > a > span');
   });
 
-  it("Page publications links", () => {
+  it("/publications links", () => {
     cy.visit("/publications")
       .get('[data-cy="link"] > a')
       .each((link) => {
@@ -31,7 +31,43 @@ describe("Links", () => {
       });
   });
 
-  it("Page publication links text", () => {
-    cy.visit("/").get('[data-cy="link"] > a > span');
+  it("/publication links text", () => {
+    cy.visit("/publications").get('[data-cy="link"] > a > span');
+  });
+
+  it("/publications/articles links", () => {
+    cy.visit("/publications/articles")
+      .get('[data-cy="link"] > a')
+      .each((link) => {
+        cy.get(link)
+          .should("have.attr", "href")
+          .then((href) => {
+            cy.request(href).then((resp) => {
+              expect(resp.status).to.eq(200);
+            });
+          });
+      });
+  });
+
+  it("/publication/articles links text", () => {
+    cy.visit("/publications/articles").get('[data-cy="link"] > a > span');
+  });
+
+  it("/publications/evenements links", () => {
+    cy.visit("/publications/evenements")
+      .get('[data-cy="link"] > a')
+      .each((link) => {
+        cy.get(link)
+          .should("have.attr", "href")
+          .then((href) => {
+            cy.request(href).then((resp) => {
+              expect(resp.status).to.eq(200);
+            });
+          });
+      });
+  });
+
+  it("/publications/evenements links text", () => {
+    cy.visit("/publications/evenements").get('[data-cy="link"] > a > span');
   });
 });
